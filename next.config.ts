90ee375d8af8e883a8dab05d5cd2dcba1fd2ba4f
Satyam Mishra (Technology - New Delhi) <satyam.mishra@htdigital.in>
2025-05-06 11:38:33 +0530

    update basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   
-  basePath: '/satyam-ht2023.github.io',
+  basePath: '/affiliate-hygiene-dashboard',
   images: {
     unoptimized: true,
   },
